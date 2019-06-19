3d773df799ddd15fd49488c7d9e8a1ff716d9460
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 20:42:50 +0200

    Ver detalle

--- ListaCursos/Pages/Details.cshtml.cs
+++ ListaCursos/Pages/Details.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ListaCursos.Interfaces;
 using ListaCursos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,11 +11,25 @@ namespace ListaCursos.Pages
 {
     public class DetailsModel : PageModel
     {
+        private readonly ICoursesProvider coursesProvider;
+
         public Course Course { get; set; }
 
-        public void OnGet()
+        public DetailsModel(ICoursesProvider coursesProvider)
         {
-            Course = new Course();
+            this.coursesProvider = coursesProvider;
+        }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            var course = await coursesProvider.GetAsync(id);
+            if (course != null)
+            {
+                Course = course;
+                return Page();
+            }
+
+            return RedirectToPage("Courses");
         }
     }
 }
