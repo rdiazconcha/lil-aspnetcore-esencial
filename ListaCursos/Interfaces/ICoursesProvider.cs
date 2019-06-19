20a3059ff217c90f9dc521409c3eebb21b67be21
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 14:36:06 +0200

    Modelo y proveedor de datos

---
+++ ListaCursos/Interfaces/ICoursesProvider.cs
@@ -0,0 +1,13 @@
+﻿using ListaCursos.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ListaCursos.Interfaces
+{
+    public interface ICoursesProvider
+    {
+        Task<ICollection<Course>> GetAllAsync();
+
+        Task<Course> GetAsync(int id);
+    }
+}
---
+++ ListaCursos/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaCursos.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Uri { get; set; }
+    }
+}
