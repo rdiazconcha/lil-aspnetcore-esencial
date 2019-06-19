268606120c0b7f7de4cfbd84505eb669d0d19942
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 13:49:55 +0200

    Módulos 1-4

---
+++ helloworld/LinkedInLearningMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace helloworld
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LinkedInLearningMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LinkedInLearningMiddleware> logger;
+        private readonly IConfiguration configuration;
+
+        public LinkedInLearningMiddleware(RequestDelegate next, ILogger<LinkedInLearningMiddleware> logger, IConfiguration configuration)
+        {
+            _next = next;
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            logger?.LogInformation(configuration["mensaje"]);
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class LinkedInLearningMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLinkedInLearningMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LinkedInLearningMiddleware>();
+        }
+    }
+}
---
+++ helloworld/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace helloworld.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration configuration;
+
+        public string Mensaje { get; set; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Mensaje = configuration["mensaje"];
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
