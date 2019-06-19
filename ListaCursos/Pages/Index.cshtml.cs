14b9787c1a851cc64988175e3d06ee6a77deb55b
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 14:17:59 +0200

    Página maestra Courses

---
+++ ListaCursos/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ListaCursos.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = Guid.NewGuid().ToString();
+        }
+    }
+}
