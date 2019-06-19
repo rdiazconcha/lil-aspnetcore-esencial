2eeec3cb63e3650337f6465b4666bd7527d68f8a
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 20:23:45 +0200

    Details

---
+++ ListaCursos/Pages/Details.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListaCursos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ListaCursos.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public Course Course { get; set; }
+
+        public void OnGet()
+        {
+            Course = new Course();
+        }
+    }
+}
