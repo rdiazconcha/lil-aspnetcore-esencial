960f8f8cc92760577a2d25efc5579658f36bbe12
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 21:00:30 +0200

    Edit

---
+++ ListaCursos/Pages/Edit.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListaCursos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ListaCursos.Pages
+{
+    public class EditModel : PageModel
+    {
+        [BindProperty]
+        public Course Course { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            //...
+        }
+    }
+}
