4c9e76cfefbde27864c9d5f424c0f42833c41e50
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 15:08:03 +0200

    Lista de cursos

--- ListaCursos/Pages/Courses.cshtml.cs
+++ ListaCursos/Pages/Courses.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ListaCursos.Interfaces;
+using ListaCursos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,24 @@ namespace ListaCursos.Pages
 {
     public class CoursesModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ICoursesProvider coursesProvider;
 
+        public List<Course> Courses { get; set; }
+
+        public CoursesModel(ICoursesProvider coursesProvider)
+        {
+            this.coursesProvider = coursesProvider;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var results = await coursesProvider.GetAllAsync();
+            if (results != null)
+            {
+                Courses = new List<Course>(results);
+            }
+
+            return Page();
         }
     }
 }
--- ListaCursos/Startup.cs
+++ ListaCursos/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ListaCursos.Interfaces;
+using ListaCursos.Providers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +25,8 @@ namespace ListaCursos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ICoursesProvider, FakeCoursesProvider>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
