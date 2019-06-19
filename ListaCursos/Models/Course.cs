8162a2e24614689045ffe7e3388df517ad575005
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-19 21:40:33 +0200

    Validación con Data Annotations

--- ListaCursos/Models/Course.cs
+++ ListaCursos/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,21 @@ namespace ListaCursos.Models
     public class Course
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(500, ErrorMessage = "El número máximo de caracteres permitidos es 500")]
         public string Description { get; set; }
+
+        [Display(Name = "Autor(a)")]
+        [Required(ErrorMessage = "El(la) autor(a) es requerido(a)")]
         public string Author { get; set; }
+
+        [Display(Name = "Dirección del curso")]
+        [Url(ErrorMessage = "La dirección no es válida")]
         public string Uri { get; set; }
     }
 }
--- ListaCursos/Pages/Edit.cshtml.cs
+++ ListaCursos/Pages/Edit.cshtml.cs
@@ -32,6 +32,11 @@ namespace ListaCursos.Pages
         }
         public async Task<IActionResult> OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             var result = await coursesProvider.UpdateAsync(Course.Id, Course);
 
             if (result)
