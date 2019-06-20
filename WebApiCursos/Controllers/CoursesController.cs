c602ceb8a2e474740c92477c992cd9c5571ad19e
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-20 16:30:43 +0200

    Implementación de métodos en el Controller

--- WebApiCursos/Controllers/CoursesController.cs
+++ WebApiCursos/Controllers/CoursesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApiCursos.Interfaces;
+using WebApiCursos.Models;
 
 namespace WebApiCursos.Controllers
 {
@@ -17,6 +18,73 @@ namespace WebApiCursos.Controllers
         public CoursesController(ICoursesProvider coursesProvider)
         {
             this.coursesProvider = coursesProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var results = await coursesProvider.GetAllAsync();
+            if (results != null)
+            {
+                return Ok(results);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var result = await coursesProvider.GetAsync(id);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound(id);
+        }
+
+        [HttpGet("search/{search}")]
+        public async Task<IActionResult> SearchAsync(string search)
+        {
+            var results = await coursesProvider.SearchAsync(search);
+            
+            if (results != null)
+            {
+                return Ok(results);
+            }
+
+            return NotFound(search);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(Course course)
+        {
+            if (course == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await coursesProvider.AddAsync(course);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Id);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")] //api/courses/1
+        public async Task<IActionResult> UpdateAsync(int id, Course course)
+        {
+            var result = await coursesProvider.UpdateAsync(id, course);
+            if (result)
+            {
+                return Ok();
+            }
+
+            return NotFound();
         }
     }
 }
