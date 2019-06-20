504533c2a1c51098f8586fa7667d6152a6e1343a
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-20 14:55:17 +0200

    CoursesController

---
+++ WebApiCursos/Controllers/CoursesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCursos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 10;
+        }
+    }
+}
