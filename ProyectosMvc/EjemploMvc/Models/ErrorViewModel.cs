026fa6f86b7fc107cab5ebf4678987bd47fd3de6
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 15:25:09 +0200

    Ejemplos MVC

---
+++ ProyectosMvc/EjemploMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EjemploMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
